d65c48a9408e2fb0a427f2c3b2c5ee3f0a3df153
vladanee <vlacvetkov@gmail.com>
2024-03-25 13:56:01 +0100

    FixedBaseCard

--- src/stories/BaseCard.tsx
+++ src/stories/BaseCard.tsx
@@ -1,42 +1,38 @@
 import React from 'react';
-import { CardContent, Typography } from '@mui/material'
-import { GatsbyImage } from "gatsby-plugin-image"
-import { IGatsbyImageData, StaticImage } from 'gatsby-plugin-image';
-import './cardStyles.css'
+import { CardContent, Typography } from '@mui/material';
+import './cardStyles.css';
+import Image from 'next/image';
 
 interface CardProps {
     title: string;
     date?: any;
-    thumbnail?: IGatsbyImageData;
+    thumbnail?: string; // Assuming thumbnail is a URL string
 }
 
 const BaseCard: React.FC<CardProps> = ({ title, date, thumbnail }) => {
     const renderThumbnail = (thumbnailSrc: string | undefined, alt: string) => {
         if (thumbnailSrc) {
+            // Using Next.js Image component for optimized image loading
             return (
-                <img
-                    className="thumbnail"
+                <Image
                     src={thumbnailSrc}
                     alt={alt}
-                    style={{
-                        width: '100%',
-                        height: 200,
-                        objectFit: 'cover',
-                    }}
+                    width={500} // Specify width
+                    height={300} // Specify height
+                    objectFit="cover"
+                    className="thumbnail"
                 />
             );
         } else {
             // Fallback to a default image if `thumbnailSrc` is not provided
             return (
-                <img
+                <Image
                     src="https://i.ibb.co/Bq2J6JT/Static-Thumbnail.png" // Default or placeholder image URL
-                    className="thumbnail"
-                    alt="Placeholder"
-                    style={{
-                        width: '100%',
-                        height: 200,
-                        objectFit: 'cover',
-                    }}
+                    alt='Placeholder'
+                    width={500}
+                    height={300}
+                    objectFit="cover"
+                    className='thumbnail'
                 />
             );
         }
@@ -53,7 +49,8 @@ const BaseCard: React.FC<CardProps> = ({ title, date, thumbnail }) => {
                         color: "#383D3B",
                         fontSize: { xs: 18, sm: 18, md: 20, lg: 20, xl: 22 },
                     }}
-                    component="h2">
+                    component="h2"
+                >
                     {title}
                 </Typography>
                 <Typography
@@ -61,14 +58,14 @@ const BaseCard: React.FC<CardProps> = ({ title, date, thumbnail }) => {
                     fontWeight={300}
                     sx={{
                         color: "#383D3B",
-                        fontSize: { xs:15, sm: 15, md: 17, lg: 17, xl: 19 },
+                        fontSize: { xs: 15, sm: 15, md: 17, lg: 17, xl: 19 },
                     }}
                 >
-                    {new Date(date).toLocaleDateString()}
+                    {date ? new Date(date).toLocaleDateString() : ""}
                 </Typography>
             </>
         );
-    }
+    };
 
     return (
         <>
